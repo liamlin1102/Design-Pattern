809daf11b9fcaba0d92995a2e6fdd4e91ecf9a94
liamlin <linliam1102@gmail.com>
2024-05-09 02:32:14 +0800

    feat: 大老二 新增檯面以及OOA

--- BigTwo/Rank.py
+++ BigTwo/Rank.py
@@ -1,6 +1,6 @@
 from enum import Enum,auto
 class Rank(Enum):
-    club =0
-    diamond = auto()
-    heart = auto()
-    spade = auto()
+    Club =0
+    Diamond = auto()
+    Heart = auto()
+    Spade = auto()
---
+++ BigTwo/Surface.py
@@ -0,0 +1,8 @@
+from CardPattern import CardPattern
+from Player import Player
+
+class Surface:
+    def __init__(self,topPlay:CardPattern=None,topPlayer:Player=None):
+        self.topPlay = self.topPlay
+        self.topPlayer = self.topPlayer
+    
